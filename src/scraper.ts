c42cd786a10f0fc2c8323938f4e49a7555b0bded
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-09-29 10:01:53 +0100

    Add some more fetch headers to help improve scraping

--- src/scraper.ts
+++ src/scraper.ts
@@ -26,7 +26,11 @@ class Scraper {
     this.url = url
     this.response = await fetch(url, {
       headers: {
+        referrer: 'http://www.google.com/',
         'User-Agent': randomUserAgent(),
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
       },
     })
 
