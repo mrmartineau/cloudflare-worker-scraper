ae329238f75e09585488e9a5d43d09050148af5e
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-11-09 15:35:10 +0000

    Update scraper logic
    Switch to use `for…of` loop instead of nested `forEach` so we can `break` out of them

--- src/scraper.ts
+++ src/scraper.ts
@@ -59,66 +59,75 @@ class Scraper {
   async getMetadata(
     options: GetMetadataOptions[]
   ): Promise<Record<string, string | string[]>> {
-    const matches: Record<string, string | string[]> = {}
+    let matches: Record<string, string | string[]> = {}
+    let selectedSelectors: Record<string, boolean> = {}
 
-    for (let index = 0; index < options.length; index++) {
-      const name = options[index].name
-      const isMultiple = options[index].multiple
-      if (isMultiple) {
-        matches[name] = []
-      } else {
-        matches[name] = ''
+    for (const optionsItem of options) {
+      const name = optionsItem.name
+      const isMultiple = optionsItem.multiple
+
+      if (!matches[name]) {
+        if (isMultiple) {
+          matches[name] = []
+        } else {
+          matches[name] = ''
+        }
       }
-      options[index].selectors.forEach((item: GetValueOption) => {
+
+      selectorLoop: for await (const item of optionsItem.selectors) {
         const selector = item.selector
         let nextText = ''
 
-        if (item.attribute) {
-          // Get attribute value
-          this.rewriter.on(selector, {
-            element(element) {
-              if (item.attribute) {
-                const attrText = element.getAttribute(item.attribute)
-                if (attrText) {
-                  nextText = attrText
+        if (selectedSelectors[name]) {
+          break selectorLoop
+        }
 
-                  // If multiple, push to array, otherwise set as string
-                  if (isMultiple) {
-                    matches[name].push(cleanText(nextText))
-                  } else {
-                    if (matches[name] === '') {
-                      matches[name] = cleanText(nextText)
-                    }
+        this.rewriter.on(selector, {
+          element(element: Element) {
+            if (item.attribute) {
+              // Get attribute content value
+
+              const attrText = element.getAttribute(item.attribute)
+              if (attrText) {
+                nextText = attrText
+
+                // If multiple, push to array, otherwise set as string
+                if (isMultiple) {
+                  Array.isArray(matches[name]) &&
+                    (matches[name] as string[]).push(cleanText(nextText))
+                } else {
+                  if (matches[name] === '') {
+                    matches[name] = cleanText(nextText)
+                    selectedSelectors[name] = true
                   }
                 }
               }
-            },
-          })
-        } else {
-          // Get text content value
-          this.rewriter.on(selector, {
-            element() {
+            } else {
               nextText = ''
-            },
-            text(text) {
+            }
+          },
+          text(text) {
+            // Get text content value
+            if (!item.attribute) {
               nextText += text.text
 
               if (text.lastInTextNode) {
                 // If multiple, push to array, otherwise set as string
                 if (isMultiple) {
                   Array.isArray(matches[name]) &&
-                    matches[name].push(cleanText(nextText))
+                    (matches[name] as string[]).push(cleanText(nextText))
                 } else {
                   if (matches[name] === '') {
                     matches[name] = cleanText(nextText)
+                    selectedSelectors[name] = true
                   }
                 }
                 nextText = ''
               }
-            },
-          })
-        }
-      })
+            }
+          },
+        })
+      }
     }
     const transformed = this.rewriter.transform(this.response)
     await transformed.arrayBuffer()
